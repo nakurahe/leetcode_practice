60c5cb5b80580463fa41348fe8b67d412c954b3a
Nakura <126201955+nakurahe@users.noreply.github.com>
2024-11-14 13:29:37 -0800

    added complexity

--- java/338_counting_bits.java
+++ java/338_counting_bits.java
@@ -1,9 +1,10 @@
-// 338. Counting Bits
 // Tried to calculate the binary representation of each number
 // and count the number of 1s in it. Failed.
 
-// Best Solution:
+// Better Solution:
 // Referred from: https://leetcode.com/problems/counting-bits/solutions/6032841/0-ms-runtime-beats-100-user-step-by-steps-solution-easy-to-understand
+// Time complexity: O(n)
+// Space complexity: O(n)
 class Solution {
     public int[] countBits(int n) {
         int[] ans = new int[n + 1];
